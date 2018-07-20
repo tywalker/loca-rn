c044e0ef261f32ebce4aaf58b91d1b1bdc9d2e8d
Tyler Walker <tyler.walker@dakno.com>
2018-07-19 21:57:06 -0400

    Basic starting view

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,11 +19,34 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   render() {
+    let exArr = [];
+    for (var i = 0; i < 20; i++) {
+      exArr.push(i);
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={ styles.topNavigation }>
+
+        </View>
+        <View style={ styles.mainViewContainer }>
+          <ScrollView style={ styles.locationsContainer }>
+            {
+              exArr.map( (item, index) => {
+                return (
+                  <View key={index} style={ styles.location }>
+                    <Text style={ styles.locaLabel }>Label { item }</Text>
+                    <View style={ styles.locaImageContainer }>
+                      <Image
+                        source={{ uri: "https://placeimg.com/200/150/any" }}
+                        style={{ height: 150, width: 200 }}
+                      />
+                    </View>
+                  </View>
+                )
+              })
+            }
+          </ScrollView>
+        </View>
       </View>
     );
   }
@@ -32,18 +55,32 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: 'column',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  topNavigation: {
+    flex: 0.2,
+    backgroundColor: 'pink'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  mainViewContainer: {
+    flex: 0.8,
+    backgroundColor: 'blue',
+    alignItems: 'center'
   },
+  locationsContainer: {
+    flex: 1,
+    minWidth: '90%',
+    maxWidth: '90%',
+    backgroundColor: 'green'
+  },
+  location: {
+    height: 300,
+  },
+  locaLabel: {
+
+  },
+  locaImageContainer: {
+    minHeight: 150,
+    minWidth: 200
+  }
 });
