af11124d08e437d0ec18d9435a21d78493667aab
Tyler Walker <tyler.walker@dakno.com>
2018-07-20 23:58:22 -0400

    Added function to consume places data from bbox flickr endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -8,6 +8,7 @@ export const fetchExample = () => {
 const getBoundingBoxFromGeo = () => {
   let latInSeconds = diameter * 0.5 / 69;
   let lonInSeconds = diameter * 0.5 / (30.9 * Math.cos(51.503364051));
+
   let lonUpper = location["lon"]! + lonInSeconds * 0.5;
   let latUpper = location["lat"]! + latInSeconds * 0.5;
   let lonLower = location["lon"]! - lonInSeconds * 0.5;
@@ -15,4 +16,34 @@ const getBoundingBoxFromGeo = () => {
 
   let bndBox = "\(lonLower),\(latLower),\(lonUpper),\(latUpper)";
   return bndBox;
+}
+
+export const getPlacesFromBB = (bbox, distance) => {
+  let baseUrl = "https://api.flickr.com/services/rest/?method=flickr.places.placesForBoundingBox"
+  var place_type_id = 0
+
+  switch distance {
+  case 0...1.8:
+     place_type_id = 22
+  case 1.81...4.3:
+     place_type_id = 7
+  case 4.31...31:
+     place_type_id = 9
+  case 31.1...124:
+     place_type_id = 8
+  case 124.1...310:
+      place_type_id = 12
+  default:
+      print("Does not conform")
+  }
+
+  let defaultParams = [
+      "api_key" : "2b3fe0a28145f01a2ab0a1ae3ee65c1d",
+      "bbox": bbox,
+      "place_type_id": place_type_id.toString(),
+      "format" : "json",
+      "nojsoncallback" : "1",
+  ]
+
+  let url = buildUrlString(baseUrl: baseUrl, params: defaultParams) as! URL
 }
